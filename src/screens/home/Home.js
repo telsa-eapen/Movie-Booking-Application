ca5890f518e0b114663de17ad5bb23b2203d63ba
telsa-eapen <telsaanna15@gmail.com>
2021-05-08 16:48:28 +0530

    Checkpoint4

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Header from '../../common/header/Header.js';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
+import Header from '../../common/header/Header.js';import Select from '@material-ui/core/Select';
+import GridList from '@material-ui/core/GridList';import MenuItem from '@material-ui/core/MenuItem';
 import moviesData from '../../common/moviesData.js';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';import genres from '../../common/genre.js';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 class Home extends Component
 {
     render(){
@@ -39,6 +43,28 @@ class Home extends Component
             }
         </GridList>
         </div>
+        <Card>
+            <CardContent>FIND MOVIES BY:</CardContent>
+            <CardActions>
+                <FormControl>
+                
+      <TextField id="standard-basic" label="Movie Name" />
+      <Select label="Genres">
+          {
+              genres.map((genre) =>
+          
+          <MenuItem value={genre.id}>{genre.name}</MenuItem>
+          )}
+        </Select>
+        <FormControl label="Release Start Date"></FormControl>
+        <TextField type="date" ></TextField>
+      
+        <Button variant="contained" color="primary" disableElevation>
+  APPLY
+</Button>
+                </FormControl>
+            </CardActions>
+        </Card>
         </div>
           </div>
         );
